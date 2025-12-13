84922029a78c95c905d78342b3233027ad079762
Cande Echazú <87193825+cande342@users.noreply.github.com>
2025-12-13 15:40:45 -0300

    Refactor set-env.js for clarity and organization

--- set-env.js
+++ set-env.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-// Definimos la ruta donde Angular espera el archivo
+
 const dir = './src/environments';
 const fileName = 'environment.ts';
 const filePath = path.join(dir, fileName);
 
-// Creamos el contenido usando variables de entorno que pondremos en Netlify
+
 const envConfigFile = `
 export const environment = {
   production: true,
@@ -21,12 +21,11 @@ export const environment = {
 };
 `;
 
-// 1. Nos aseguramos de que la carpeta existe
+
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir, { recursive: true });
 }
 
-// 2. Escribimos el archivo
+
 fs.writeFileSync(filePath, envConfigFile);
 
-console.log(`✅ Archivo ${fileName} generado correctamente en ${dir}`);
